c76f9e2d024191bcd4222712cba9ac51db2d375c
rossim2i2 <rossim2i2@gmail.com>
2025-12-19 21:01:55 -0500

    more symlinks for niri waybar wezterm

--- config/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,7 +12,7 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} }, -- LSP/DAP/Linter installer & manager
-		-- "creativenull/efmls-configs-nvim", -- Preconfigured EFM Language Server setups
+		"creativenull/efmls-configs-nvim" -- Preconfigured EFM Language Server setups
 		-- "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
 	},
 	config = function()
