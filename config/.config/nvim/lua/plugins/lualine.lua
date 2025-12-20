4deabe8b0c11760dc5ce85e8775fd762d246f60c
rossim2i2 <rossim2i2@gmail.com>
2025-12-20 12:34:41 -0500

    finished lsps for now

--- config/.config/nvim/lua/plugins/lualine.lua
+++ config/.config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "tokyonight",
+        theme = "tokyonight"
         icons_enabled = true,
         section_separators = "",
         component_separators = "",
