9637281ba5d40686826cc5764b061bcc9350e697
rossim2i2 <rossim2i2@gmail.com>
2025-12-20 08:57:07 -0500

    continued changes to nvim configs

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -51,15 +51,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 --})
 
 -- open terminal in insert mode
-vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-    vim.cmd("startinsert")
-  end,
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+		vim.cmd("startinsert")
+	end,
 })
 
 vim.keymap.set("n", "<space>st", function()
-  vim.cmd("botright 15split | terminal")
+	vim.cmd("botright 15split | terminal")
 end)
--- config/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,8 +12,8 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} }, -- LSP/DAP/Linter installer & manager
-		"creativenull/efmls-configs-nvim" -- Preconfigured EFM Language Server setups
-		-- "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
+		"creativenull/efmls-configs-nvim", -- Preconfigured EFM Language Server setups
+		"hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
 	},
 	config = function()
 		require("utils.diagnostics").setup()
