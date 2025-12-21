cf794b7a5f5700d383f9cf2dcbf2a35034711014
rossim2i2 <rossim2i2@gmail.com>
2025-12-21 11:37:54 -0500

    added zet auto commit and fuzzy find

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,38 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = 'v0.2.0',
-  dependencies = { "nvim-lua/plenary.nvim",
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-  },
-  config = function()
-    local telescope = require("telescope")
-    telescope.setup({
-      pickers = {
-        find_files = {
-          theme = "ivy"
-        },
-        extentions = {
-          fzf = {}
-        }
-      }
-    })
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
+	dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			pickers = {
+				find_files = {
+					theme = "ivy",
+				},
+				extentions = {
+					fzf = {},
+				},
+			},
+		})
 
-    require('telescope').load_extension('fzf')
+		require("telescope").load_extension("fzf")
 
-    local builtin = require("telescope.builtin")
-    local map = vim.keymap.set
+		local builtin = require("telescope.builtin")
+		local map = vim.keymap.set
 
-    map("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-    map("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
-    map("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
-    map("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-    map("n", "<leader>en", function()
-      builtin.find_files {
-        cwd = vim.fn.stdpath("config")
-      }
-    end)
-  end
+		map("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		map("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+		map("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
+		map("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+		map("n", "<leader>en", function()
+			builtin.find_files({
+				cwd = vim.fn.stdpath("config"),
+			})
+		end)
+		map("n", "<leader>fz", function()
+			builtin.live_grep({
+				cwd = "~/Repos/github.com/rossim2i2/zet",
+			})
+		end)
+	end,
 }
--- config/.config/nvim/lua/plugins/zk.lua
+++ config/.config/nvim/lua/plugins/zk.lua
@@ -1,60 +1,139 @@
 return {
-  "zk-org/zk-nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
+	"zk-org/zk-nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
 
-  config = function()
-    require("zk").setup({
-      picker = "telescope",
-      lsp = {
-        config = {
-          name = "zk",
-          cmd = { "zk", "lsp" },
-          filetypes = { "markdown" },
-        },
-        auto_attach = {
-          enabled = true,
-        },
-      },
-    })
+	config = function()
+		require("zk").setup({
+			picker = "telescope",
+			lsp = {
+				config = {
+					name = "zk",
+					cmd = { "zk", "lsp" },
+					filetypes = { "markdown" },
+				},
+				auto_attach = {
+					enabled = true,
+				},
+			},
+		})
 
-    local zk_cmd = require("zk.commands")
-    local zet_path = vim.fn.expand("~/zet")
+		local zk_cmd = require("zk.commands")
+		local zet_path = vim.fn.expand("~/Repos/github.com/rossim2i2/zet")
 
-    local function map(lhs, rhs, desc)
-      vim.keymap.set("n", lhs, rhs, {
-        noremap = true,
-        silent = true,
-        desc = desc,
-      })
-    end
+		-- Returns true if `file` is inside `root` (both resolved)
+		local function is_in_dir(file, root)
+			file = vim.fn.fnamemodify(vim.fn.resolve(file), ":p")
+			root = vim.fn.fnamemodify(vim.fn.resolve(root), ":p")
+			return file:sub(1, #root) == root
+		end
 
-    -- New note (always in ~/zet)
-    map("<leader>zn", function()
-      zk_cmd.get("ZkNew")({ notebook_path = zet_path })
-    end, "Zk: New note")
+		-- Run a git command in the zet repo
+		local function git(args)
+			return vim.system(vim.list_extend({ "git", "-C", zet_path }, args), { text = true })
+		end
 
-    -- Open notes (Telescope, always uses ~/zet)
-    map("<leader>zo", function()
-      zk_cmd.get("ZkNotes")({
-        sort = { "modified" },
-        notebook_path = zet_path,
-      })
-    end, "Zk: Open note")
+		-- Debounced push so you don’t push 50 times while editing
+		local push_timer = vim.uv.new_timer()
+		local function schedule_push()
+			push_timer:stop()
+			push_timer:start(1500, 0, function()
+				vim.schedule(function()
+					-- Push quietly; if it fails (offline/auth), we just notify.
+					git({ "push" }):wait()
+				end)
+			end)
+		end
 
-    -- Grep notes
-    map("<leader>zg", function()
-      zk_cmd.get("ZkGrep")({
-        notebook_path = zet_path,
-      })
-    end, "Zk: Grep notes")
+		-- Commit + push current note (only if there are changes)
+		local function commit_and_push(bufnr)
+			local file = vim.api.nvim_buf_get_name(bufnr)
+			if file == "" or not is_in_dir(file, zet_path) then
+				return
+			end
 
-    -- Browse tags
-    map("<leader>zt", function()
-      zk_cmd.get("ZkTags")({
-        notebook_path = zet_path,
-      })
-    end, "Zk: Tags")
-  end,
+			-- Only act on markdown notes
+			if vim.bo[bufnr].filetype ~= "markdown" then
+				return
+			end
+
+			-- If nothing changed, do nothing
+			local status = git({ "status", "--porcelain" }):wait()
+			if status.code ~= 0 then
+				vim.notify("zk git status failed:\n" .. (status.stderr or ""), vim.log.levels.WARN)
+				return
+			end
+			if (status.stdout or ""):match("^%s*$") then
+				return
+			end
+
+			-- Stage changes
+			local add = git({ "add", "--", "." }):wait()
+			if add.code ~= 0 then
+				vim.notify("zk git add failed:\n" .. (add.stderr or ""), vim.log.levels.WARN)
+				return
+			end
+
+			-- Commit with a nice message
+			local msg = ("zk: %s"):format(vim.fn.fnamemodify(file, ":t"))
+			local commit = git({ "commit", "-m", msg }):wait()
+
+			-- If commit fails because there’s nothing to commit, ignore
+			if commit.code ~= 0 then
+				local err = (commit.stderr or "") .. (commit.stdout or "")
+				if not err:match("nothing to commit") then
+					vim.notify("zk git commit failed:\n" .. err, vim.log.levels.WARN)
+				end
+				return
+			end
+
+			-- Push (debounced)
+			schedule_push()
+		end
+
+		-- Autocmd: on save, commit+push if this file is inside zet repo
+		vim.api.nvim_create_augroup("ZkAutoGit", { clear = true })
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			group = "ZkAutoGit",
+			callback = function(args)
+				commit_and_push(args.buf)
+			end,
+		})
+
+		local function map(lhs, rhs, desc)
+			vim.keymap.set("n", lhs, rhs, {
+				noremap = true,
+				silent = true,
+				desc = desc,
+			})
+		end
+
+		-- New note (always in ~/zet)
+		map("<leader>zn", function()
+			zk_cmd.get("ZkNew")({ notebook_path = zet_path })
+		end, "Zk: New note")
+
+		-- Open notes (Telescope, always uses ~/zet)
+		map("<leader>zo", function()
+			zk_cmd.get("ZkNotes")({
+				sort = { "modified" },
+				notebook_path = zet_path,
+			})
+		end, "Zk: Open note")
+
+		-- Grep notes
+		map("<leader>zg", function()
+			zk_cmd.get("ZkGrep")({
+				notebook_path = zet_path,
+			})
+		end, "Zk: Grep notes")
+
+		-- Browse tags
+		map("<leader>zt", function()
+			zk_cmd.get("ZkTags")({
+				notebook_path = zet_path,
+			})
+		end, "Zk: Tags")
+	end,
 }
