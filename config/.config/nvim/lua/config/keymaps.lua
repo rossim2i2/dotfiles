a3b1e8fca92540091ae7bf912643b5e7b102546e
rossim2i2 <rossim2i2@gmail.com>
2025-12-23 08:59:36 -0500

    keymaps for inbox

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -65,23 +65,55 @@ local ai_guard = require("utils.ai_guard")
 
 -- gen.nvim (visual transforms)
 vim.keymap.set("v", "<leader>za", function()
-  if not ai_guard.ensure_in_zet() then return end
-  vim.cmd("Gen ZK_Atomic")
+	if not ai_guard.ensure_in_zet() then
+		return
+	end
+	vim.cmd("Gen ZK_Atomic")
 end, { desc = "ZK: atomic notes from selection" })
 
 vim.keymap.set("v", "<leader>zt", function()
-  if not ai_guard.ensure_in_zet() then return end
-  vim.cmd("Gen ZK_TitleTags")
+	if not ai_guard.ensure_in_zet() then
+		return
+	end
+	vim.cmd("Gen ZK_TitleTags")
 end, { desc = "ZK: title + tags from selection" })
 
 vim.keymap.set("v", "<leader>zr", function()
-  if not ai_guard.ensure_in_zet() then return end
-  vim.cmd("Gen ZK_Rewrite")
+	if not ai_guard.ensure_in_zet() then
+		return
+	end
+	vim.cmd("Gen ZK_Rewrite")
 end, { desc = "ZK: rewrite selection" })
 
 -- optional: open Gen prompt picker
 vim.keymap.set({ "n", "v" }, "<leader>zg", function()
-  if not ai_guard.ensure_in_zet() then return end
-  vim.cmd("Gen")
+	if not ai_guard.ensure_in_zet() then
+		return
+	end
+	vim.cmd("Gen")
 end, { desc = "ZK: Gen prompt picker" })
 
+--Process Zet Inbox
+vim.keymap.set("n", "<leader>ip", function()
+	require("config.inbox").process_current_inbox()
+end, { desc = "Process inbox item" })
+
+-- native neovim keymaps
+vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>") -- goto definition
+vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>") -- goto definition
+vim.keymap.set("n", "<leader>gS", "<cmd>vsplit | Lspsaga goto_definition<CR>") -- goto definition in split
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>") -- Code actions
+vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>") -- Rename symbol
+vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>") -- Line diagnostics (float)
+vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>") -- Cursor diagnostics
+vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>") -- previous diagnostic
+vim.keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>") -- next diagnostic
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>") -- hover documentation
+
+-- fzf-lua keymaps
+vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua lsp_finder<CR>") -- LSP Finder (definition + references)
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua lsp_references<CR>") -- Show all references to the symbol under the cursor
+vim.keymap.set("n", "<leader>ft", "<cmd>FzfLua lsp_typedefs<CR>") -- Jump to the type definition of the symbol under the cursor
+vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>") -- List all symbols (functions, classes, etc.) in the current file
+vim.keymap.set("n", "<leader>fs", "<cmd>FzfLua lsp_workspace_symbols<CR>") -- Search for any symbol across the entire project/workspace
+vim.keymap.set("n", "<leader>fi", "<cmd>FzfLua lsp_implementations<CR>") -- Go to implementation
