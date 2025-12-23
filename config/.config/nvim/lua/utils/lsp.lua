9d2c298d7ec41ce47ebfcd735c4fae3c579ff77a
rossim2i2 <rossim2i2@gmail.com>
2025-12-23 09:02:06 -0500

    lsp show diag keymap to sD

--- config/.config/nvim/lua/utils/lsp.lua
+++ config/.config/nvim/lua/utils/lsp.lua
@@ -19,8 +19,8 @@ M.on_attach = function(event)
 	keymap("n", "<leader>gS", "<cmd>vsplit | Lspsaga goto_definition<CR>", opts) -- goto definition in split
 	keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- Code actions
 	keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- Rename symbol
-	keymap("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- Line diagnostics (float)
-	keymap("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- Cursor diagnostics
+	keymap("n", "<leader>sD", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- Line diagnostics (float)
+	keymap("n", "<leader>sd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- Cursor diagnostics
 	keymap("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- previous diagnostic
 	keymap("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- next diagnostic
 	keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- hover documentation
